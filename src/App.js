165df69ff9f9840abebb896eaf84c5b1fb7ff1ae
Nghia Nguyen <aihgn224@gmail.com>
2022-04-17 13:23:56 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react'
+// Make sure to import the component we just built:
+import ColorBlock from './ColorBlock'
+
+function App(){
+    let colors = [
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ]
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} />
+        )
+    })
+
+    return (
+      <div className="App">
+      {colors.map((color, i) => 
+          <ColorBlock key= color={color} />
+      )}
+  </div>
+  
+    )
+}
+
+
+export default App;
